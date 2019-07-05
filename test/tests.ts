ec3f7994c9c359b7a6ba5423c3b3f75b8804b28f
Patricio Palladino <email@patriciopalladino.com>
2019-07-05 15:08:40 -0300

    Update top-level tests

--- test/tests.ts
+++ test/tests.ts
@@ -4,7 +4,10 @@ import { assert } from "chai";
 
 import { useEnvironment } from "./helpers";
 
-describe("Gas Reporter (Truffle plugin)", function() {
+// Only one of these tests can be run for now. TASK_TEST kills the process
+// after running mocha. This should be fixed in Buidler v1.0.0-beta.9
+
+describe.skip("Gas Reporter (Truffle plugin)", function() {
   useEnvironment(__dirname + "/buidler-truffle-project");
 
   it("using option - onlyCalledMethods: false", async function() {
@@ -12,10 +15,7 @@ describe("Gas Reporter (Truffle plugin)", function() {
   });
 });
 
-// This test has a typescript compilation error complaining that
-// it can't resolve ethers from @nomiclabs/buidler. Base project
-// (ethers-example) works fine on its own though.
-describe.skip("Gas Reporter (Ethers plugin)", function() {
+describe("Gas Reporter (Ethers plugin)", function() {
   useEnvironment(__dirname + "/buidler-ethers-project");
 
   it("no options", async function() {
