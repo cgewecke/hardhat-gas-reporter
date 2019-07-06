87d471835eeb9644af9f5e7df5e2224374453e08
cgewecke <christophergewecke@gmail.com>
2019-07-06 10:28:50 -0700

    Remove truffle skip

--- test/tests.ts
+++ test/tests.ts
@@ -7,7 +7,7 @@ import { useEnvironment } from "./helpers";
 // Only one of these tests can be run for now. TASK_TEST kills the process
 // after running mocha. This should be fixed in Buidler v1.0.0-beta.9
 
-describe.skip("Gas Reporter (Truffle plugin)", function() {
+describe("Gas Reporter (Truffle plugin)", function() {
   useEnvironment(__dirname + "/buidler-truffle-project");
 
   it("using option - onlyCalledMethods: false", async function() {
