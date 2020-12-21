697436c47c518ff33e6e1450efb6186ef98766a4
cgewecke <christophergewecke@gmail.com>
2020-12-21 14:44:32 -0800

    Support Waffle wallets / eth_sendRawTransaction

---
+++ test/hardhat-waffle-project/hardhat.config.ts
@@ -0,0 +1,11 @@
+// We load the plugin here.
+import { HardhatUserConfig } from "hardhat/types";
+import "@nomiclabs/hardhat-waffle";
+
+import "../../src/index";
+
+const config: HardhatUserConfig = {
+  solidity: "0.5.8",
+};
+
+export default config;
