ed609462f4153335b3b48d1a74590c6f3807000b
cgewecke <christophergewecke@gmail.com>
2024-03-14 12:12:06 -0700

    Revert blobBaseFee change

--- src/utils/ui.ts
+++ src/utils/ui.ts
@@ -201,9 +201,9 @@ export function getCommonTableVals(options: GasReporterOptions) {
   const usingL1 = options.L2 === undefined;
 
   let token = "";
-  let l1gwei: string | number = (usingL1) ? options.gasPrice!: options.blobBaseFee!;
+  let l1gwei: string | number = (usingL1) ? options.gasPrice!: options.baseFee!;
   let l2gwei: string | number = (usingL1) ? "" : options.gasPrice!;
-  const l1gweiNote: string = (usingL1) ? "" : "(blobBaseFee)";
+  const l1gweiNote: string = (usingL1) ? "" : "(baseFee)";
   const l2gweiNote: string = (usingL1) ? "" : "(gasPrice)";
   const network = (usingL1) ? options.L1!.toUpperCase() : options.L2!.toUpperCase();
 
